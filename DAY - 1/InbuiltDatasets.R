11a6a832d372cb0e65dcc3d849c255b4f0e83f7f
klmsathish <klmsathish@gmail.com>
2021-09-04 10:23:04 +0530

    Day-1 Morning

---
+++ DAY - 1/InbuiltDatasets.R
@@ -0,0 +1,21 @@
+# installing Packages
+#install.packages("party")
+#install.packages("arules")
+#install.packages("Matrix")
+
+
+# Loading Packages
+library(party)
+library(arules)
+library(Matrix)
+
+
+# Data Overviw
+head(Income)
+head(readingSkills)
+head(Groceries)
+
+
+# Data Statistics
+summary(Groceries)
+summary(Income)
